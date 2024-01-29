1b09240916398ee7be66624693d479706f6d0ed3
muner12 <bakhtmuner06@gmail.com>
2024-01-29 21:47:19 +0500

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Post from './components/post/post'
 
 export default function Home() {
   return (
-    <div>
-      <h3>Home</h3>
+    <div className='w-full'>
+       <h3>Home</h3>
+      <div className='flex gap-x-10'>
       <ul>
         <li><Link href={"/products"}>Products</Link></li>
       </ul>
+      </div>
+
+      <div>
+
+        <Post/>
+      </div>
+     
+      
+     
 
     </div>
   )
