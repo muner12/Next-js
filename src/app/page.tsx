bc7a606a5937f79f1645ed5ebdf2817dab815c56
muner12 <bakhtmuner06@gmail.com>
2024-01-31 22:58:57 +0500

    new channges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,18 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className='w-full'></div>
+    <div className='w-full'>
+      
+
+      <div>
+
+      <ul>
+        <li><Link href={"/product"}>Product</Link></li>
+        <li><Link href={"/docs"}>Docs</Link></li>
+      </ul>
+
+      </div>
+
+    </div>
   )
 }
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -4,10 +4,13 @@ import React from 'react'
 function page() {
   return (
     <div>
-
+        <h1 className='w-full text-4xl bg-blue-300 rounded '><Link href={"/"}>Home</Link></h1>
         <ul>
 
-            <li>1</li>
+            <li>Proudct 1</li>
+            <li>Proudct 2</li>
+            <li>Proudct 3</li>
+            <li>Proudct 4</li>
         </ul>
     </div>
   )
